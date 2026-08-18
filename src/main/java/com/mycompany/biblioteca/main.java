5c9b9ab59c81aef39f25a7ea400ea19d0260429d
yanelis1612 <ypatriciagonzalez@unicesar.edu.co>
2026-08-18 00:23:40 -0500

    feat: inicializar proyecto Maven de biblioteca

---
+++ src/main/java/com/mycompany/biblioteca/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.biblioteca;
+
+/**
+ *
+ * @author yanelis
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
